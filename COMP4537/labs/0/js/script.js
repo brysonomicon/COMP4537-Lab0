8cb45ab39c9775bdc5c7260e1863cc014dc37e4b
Bryson Lindy <bslindy@gmail.com>
2025-09-06 18:40:31 -0700

    added domcontentloaded and favicon

--- COMP4537/labs/0/js/script.js
+++ COMP4537/labs/0/js/script.js
@@ -1,4 +1,15 @@
 import { GameController } from "./GameController.js"
 import { Messages } from "../lang/messages/en/user.js"
 
-new GameController(Messages);
+
+async function init() 
+{
+    if (document.readyState === 'loading')
+    {
+        await new Promise(resolve => document.addEventListener("DOMContentLoaded", resolve, { once: true }));
+    }
+
+    new GameController(Messages);
+}
+
+init();
